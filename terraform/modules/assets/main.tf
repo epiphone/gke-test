4c823fbeb008797c7d185c35b8889e1155da050e
Aleksi Pekkala <aleksi.v.a.pekkala@student.jyu.fi>
2019-02-07 17:55:36 +0200

    update CI

--- terraform/modules/assets/main.tf
+++ terraform/modules/assets/main.tf
@@ -2,6 +2,10 @@ resource "google_storage_bucket" "assets" {
   name          = "${var.name}-bucket-${var.env}"
   location      = "${var.location}"
   storage_class = "${var.storage_class}"
+
+  website {
+    main_page_suffix = "index.html"
+  }
 }
 
 resource "google_storage_bucket_iam_binding" "assets" {
